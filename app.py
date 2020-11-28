101a4fc2aeb67a5ab042f9fb544a81c7e89b3aaf
MagnetRise <nelsonmoreira69@gmail.com>
2020-11-28 21:21:47 +0000

    28/11/2020 - N

--- app.py
+++ app.py
@@ -4,16 +4,35 @@ from flask_restful import Api
 app = Flask(__name__)
 api = Api(app)
 
-data = {
-    'page': 'horarios',
-    'access': 0
-}
-
+# data = {
+#     'page': 'horarios',
+#     'access': 0
+# }
 
 @app.route('/')
 def hello_world():
     return render_template('index.html', data=data)
 
 
-if __name__ == "__main__":
-    app.run(port=5000, debug=True)
+@app.route('/<horarios>')
+def horarioTab(horarios):
+    return render_template('index.html', data=horarios)
+
+
+@app.route('/<mapa>')
+def mapTab(mapa):
+    return render_template('index.html', data=mapa)
+
+
+@app.route('/<calendario>')
+def calendarTab(calendario):
+    return render_template('index.html', data=calendario)
+
+
+@app.route('/<definicoes>')
+def settingsTab(definicoes):
+    return render_template('index.html', data=definicoes)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
