9982b4c45c0c74d8da2e272d2ea47a521fa8b350
MagnetRise <nelsonfilipepaulosmoreira@hotmail.com>
2020-12-12 22:26:30 +0000

    Cadeiras Frontend

--- routes/administracao/cadeiras.py
+++ routes/administracao/cadeiras.py
@@ -5,11 +5,26 @@ from routes.main import confirmCredentials
 admCadeiras = Blueprint('admCadeiras', __name__, template_folder='templates')
 
 
-@admCadeiras.route('/admCadeiras')
+@admCadeiras.route('/admCadeiras', methods=['POST', 'GET'])
 def admCadeirasTab():
     returnRedirect = confirmCredentials(1, 1)
     if returnRedirect != "":
         return redirect(returnRedirect)
 
-    session['page'] = "admCadeiras"
-    return render_template('administracao/cadeiras/listagem.html')
+    url = "http://127.0.0.1:5000//cursos_"
+    data = {}
+    if request.method == 'POST':
+        url = "http://127.0.0.1:5000//cursos_?search=" + request.form.get("procurar")
+        data["form"] = request.form.get("procurar")
+
+    headers = {
+        'Authorization': 'Bearer ' + session['access_token'],
+    }
+    r = requests.request("GET", url, headers=headers, data={})
+
+    if r.status_code == 200:
+        data["cursos"] = r.json()["cursos"]
+        session['page'] = "admCadeiras"
+        return render_template('administracao/cadeiras/listagem.html', data=data)
+    else:
+        return redirect("/")
