fa23fc57551c824c1b206ec2e6c0b6ce941b456b
MagnetRise <nelsonfilipepaulosmoreira@hotmail.com>
2020-12-12 13:41:16 +0000

    JS fix Template, cursos ADM side

--- routes/administracao/cursos.py
+++ routes/administracao/cursos.py
@@ -7,9 +7,36 @@ admCursos = Blueprint('admCursos', __name__, template_folder='templates')
 
 @admCursos.route('/admCursos')
 def admCursosTab():
-    returnRedirect = confirmCredentials()
+    returnRedirect = confirmCredentials(1, 1)
     if returnRedirect != "":
         return redirect(returnRedirect)
 
     session['page'] = "admCursos"
     return render_template('administracao/cursos/listagem.html')
+
+
+@admCursos.route('/admCursos/criar')
+def admCursosCriar():
+    returnRedirect = confirmCredentials(1, 1)
+    if returnRedirect != "":
+        return redirect(returnRedirect)
+
+    return render_template('administracao/cursos/cursoCriar.html')
+
+
+@admCursos.route('/admCursos/editar')
+def admCursosEditar():
+    returnRedirect = confirmCredentials(1, 1)
+    if returnRedirect != "":
+        return redirect(returnRedirect)
+
+    return
+
+
+@admCursos.route('/admCursos/pesquisar')
+def admCursosPesquisar():
+    returnRedirect = confirmCredentials(1, 1)
+    if returnRedirect != "":
+        return redirect(returnRedirect)
+
+    return
