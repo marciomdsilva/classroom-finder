cbedcd27bf0b1ab818b30721349821f83a7c6923
MagnetRise <nelsonfilipepaulosmoreira@hotmail.com>
2021-01-03 18:58:29 +0000

    Users

--- routes/administracao/utilizadores.py
+++ routes/administracao/utilizadores.py
@@ -90,40 +90,41 @@ def admUtilizadoresSave(userId):
     return redirect("/admUtilizadores/" + userId)
 
 
-@admUtilizadores.route('/admUtilizadores/create', methods=['POST', 'GET'])
+@admUtilizadores.route('/admUtilizadores/create', methods=['GET'])
+def admUtilizadoresCreate2():
+    returnRedirect = confirmCredentials(1, 1)
+    if returnRedirect != "":
+        return redirect(returnRedirect)
+    data = {}
+
+    return render_template("administracao/utilizadores/userCreate.html", data=data)
+
+
+@admUtilizadores.route('/admUtilizadores/create', methods=['POST'])
 def admUtilizadoresCreate():
     returnRedirect = confirmCredentials(1, 1)
     if returnRedirect != "":
         return redirect(returnRedirect)
     data = {}
 
-    url = "http://127.0.0.1:5000//user"
-    if request.method == 'POST':
-        url = "http://127.0.0.1:5000//user?search=" + request.form.get("search")
-        data["search"] = request.form.get("search")
-    #    Does Nothing
+    url = "http://127.0.0.1:5000//register"
+
+    data["name"] = request.form.get("nome")
+    data["access"] = request.form.get("acesso")
+    if (data["access"] == "2"):
+        data["cursos"] = []
+        data["cadeiras"] = []
+    else:
+        data["cursos"] = request.form.getlist("curso[]")
+        data["cadeiras"] = request.form.getlist("cadeiras[]")
+    payload = json.dumps(data)
 
     headers = {
         'Authorization': 'Bearer ' + session['access_token'],
     }
-    r = requests.request("GET", url, headers=headers, data={})
-
-    url = "http://127.0.0.1:5000//cursos_"
-    headers = {
-        'Authorization': 'Bearer ' + session['access_token'],
-    }
-    r1 = requests.request("GET", url, headers=headers, data={})
-
-    url = "http://127.0.0.1:5000//cadeiras_"
-    headers = {
-        'Authorization': 'Bearer ' + session['access_token'],
-    }
-    r2 = requests.request("GET", url, headers=headers, data={})
+    r = requests.request("POST", url, headers=headers, data=payload)
 
     if r.status_code == 200:
-        data["users"] = r.json()
-        data["cursos"] = r1.json()["cursos"]
-        data["cadeiras"] = r2.json()["cadeiras"]
-        return render_template('administracao/utilizadores/userCreate.html  ', data=data)
+        return redirect("/admUtilizadores")
     else:
         return redirect("/")
