e42988cf9a9cea72f75ad394b45e1c85b6b14d71
MagnetRise <nelsonfilipepaulosmoreira@hotmail.com>
2021-01-03 18:01:51 +0000

    Users Create

--- routes/administracao/utilizadores.py
+++ routes/administracao/utilizadores.py
@@ -88,3 +88,42 @@ def admUtilizadoresSave(userId):
     r = requests.request("PUT", url, headers=headers, data=payload)
 
     return redirect("/admUtilizadores/" + userId)
+
+
+@admUtilizadores.route('/admUtilizadores/create', methods=['POST', 'GET'])
+def admUtilizadoresCreate():
+    returnRedirect = confirmCredentials(1, 1)
+    if returnRedirect != "":
+        return redirect(returnRedirect)
+    data = {}
+
+    url = "http://127.0.0.1:5000//user"
+    if request.method == 'POST':
+        url = "http://127.0.0.1:5000//user?search=" + request.form.get("search")
+        data["search"] = request.form.get("search")
+    #    Does Nothing
+
+    headers = {
+        'Authorization': 'Bearer ' + session['access_token'],
+    }
+    r = requests.request("GET", url, headers=headers, data={})
+
+    url = "http://127.0.0.1:5000//cursos_"
+    headers = {
+        'Authorization': 'Bearer ' + session['access_token'],
+    }
+    r1 = requests.request("GET", url, headers=headers, data={})
+
+    url = "http://127.0.0.1:5000//cadeiras_"
+    headers = {
+        'Authorization': 'Bearer ' + session['access_token'],
+    }
+    r2 = requests.request("GET", url, headers=headers, data={})
+
+    if r.status_code == 200:
+        data["users"] = r.json()
+        data["cursos"] = r1.json()["cursos"]
+        data["cadeiras"] = r2.json()["cadeiras"]
+        return render_template('administracao/utilizadores/userCreate.html  ', data=data)
+    else:
+        return redirect("/")
